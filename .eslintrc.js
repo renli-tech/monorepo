77abec45d31d75c57a29c54d1835c4c666b1638a
Ope Abidemi Harry <opeabidemi@gmail.com>
2021-08-30 03:17:20 +0000

    chore: updated eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,32 +1,34 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es2021": true
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:jest/recommended",
+    // Prettier
+    "plugin:prettier/recommended",
+    "prettier",
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
     },
-    "extends": [
-        "eslint:recommended",
-        "plugin:@typescript-eslint/recommended",
-        // Prettier
-        "plugin:prettier/recommended",
-        "prettier",
-    ],
-    "parser": "@typescript-eslint/parser",
-    "parserOptions": {
-        "ecmaFeatures": {
-          "jsx": true
-        },
-        "ecmaVersion": 2020,
-        "sourceType": "module"
+    ecmaVersion: 2020,
+    sourceType: "module",
+  },
+  overrides: [
+    {
+      files: ["*.js"],
+      rules: {
+        "@typescript-eslint/explicit-module-boundary-types": "off",
       },
-    "overrides": [
-        {
-          "files": ["*.js"],
-          "rules": {
-            "@typescript-eslint/explicit-module-boundary-types": "off"
-          }
-        }
-      ],
-    "rules":{
-        '@typescript-eslint/no-var-requires': 0,
-    }
+    },
+  ],
+  rules: {
+    "@typescript-eslint/no-var-requires": 0,
+  },
 };
